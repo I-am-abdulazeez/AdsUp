3aa7bccb527cac92e1fc4c3bd06a4798769bf309
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-09 18:30:55 +0100

    chore: created SwiperArray

---
+++ src/data/index.ts
@@ -0,0 +1,15 @@
+import SwiperImageOne from '@/assets/images/swipe-watch.svg';
+import SwiperImageTwo from '@/assets/images/swipe-choice.svg';
+
+export const SwiperArray = [
+  {
+    id: 1,
+    text: 'Ads are annoying but AdsUp are engaging, entertaining and informative.',
+    image: SwiperImageOne,
+  },
+  {
+    id: 2,
+    text: 'Ads are annoying but AdsUp are engaging, entertaining and informative.',
+    image: SwiperImageTwo,
+  },
+];
