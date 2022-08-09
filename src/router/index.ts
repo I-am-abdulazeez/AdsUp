77877c08ed9065403f7a89e9628c50631929f55c
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-09 18:06:43 +0100

    chore: use lazy loading to import component

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,11 @@ import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 
 import TabsPage from '@/views/TabsPage.vue';
-import WelcomePage from '@/views/WelcomePage.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    component: WelcomePage,
+    component: () => import('@/views/WelcomePage.vue'),
   },
   {
     path: '/tabs/',
