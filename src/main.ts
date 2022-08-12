3c8c7fe98573262c8bd55f9543ceed9549ac95bc
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-12 09:38:58 +0100

    chore: iosTransition usage across application

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import router from './router';
 
-import { IonicVue } from '@ionic/vue';
+import { IonicVue, iosTransitionAnimation } from '@ionic/vue';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
@@ -24,9 +24,11 @@ import '@ionic/vue/css/display.css';
 import './theme/variables.css';
 
 const app = createApp(App)
-  .use(IonicVue)
+  .use(IonicVue, {
+    navAnimation: iosTransitionAnimation,
+  })
   .use(router);
-  
+
 router.isReady().then(() => {
   app.mount('#app');
 });
