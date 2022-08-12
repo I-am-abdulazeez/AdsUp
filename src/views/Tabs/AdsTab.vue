6bb4a0aa6777353d0b538c8f1ffb09c44fb9abb8
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-12 17:51:03 +0100

    chore: Adstab page

---
+++ src/views/Tabs/AdsTab.vue
@@ -0,0 +1,42 @@
+<template>
+  <ion-page>
+    <ion-header class="ion-no-border">
+      <ion-toolbar>
+        <ion-title>Ads</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Ads</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ExploreContainer name="Tab 2 page" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+
+export default defineComponent({
+  name: 'AdsPage',
+  components: {
+    ExploreContainer,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+  },
+});
+</script>
