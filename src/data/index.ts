5d1207294b963304a7d1caf02e57bce90b8551f2
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-15 16:50:13 +0100

    chore: removed unused Swipe data and added adsCard data

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,15 +1,52 @@
-import SwiperImageOne from '@/assets/images/swipe-watch.svg';
-import SwiperImageTwo from '@/assets/images/swipe-choice.svg';
+import { ref } from 'vue';
 
-export const SwiperArray = [
+import { AdsCardType } from '@/types/index';
+
+export const adsCard = ref<AdsCardType[]>([
   {
-    id: 1,
-    text: 'Ads are annoying but AdsUp are engaging, entertaining and informative.',
-    image: SwiperImageOne,
+    image:
+      'https://cdn.pixabay.com/photo/2016/10/13/22/30/tesla-1738969_960_720.jpg',
+    name: 'Tesla',
+    description: `Keep close to Nature's heart... and break clear away, once in awhile,
+    and climb a mountain or spend a week in the woods. Wash your spirit
+    clean.`,
+    timestamp: '1 day ago',
   },
   {
-    id: 2,
-    text: 'Ads are annoying but AdsUp are engaging, entertaining and informative.',
-    image: SwiperImageTwo,
+    image:
+      'https://cdn.pixabay.com/photo/2017/09/04/00/08/youtube-2712573_960_720.png',
+    name: 'Youtube',
+    description: `Keep close to Nature's heart...`,
+    timestamp: '1hr ago',
   },
-];
+  {
+    image:
+      'https://cdn.pixabay.com/photo/2016/04/28/18/39/gears-1359436_960_720.png',
+    name: 'Google',
+    description: `SEO is now better with google, right?`,
+    timestamp: '2hrs ago',
+  },
+  {
+    image:
+      'https://cdn.pixabay.com/photo/2017/01/22/12/07/imac-1999636_960_720.png',
+    name: 'Apple',
+    description: `At Apple, we give you the best.`,
+    timestamp: '1 day ago',
+  },
+  {
+    image:
+      'https://cdn.pixabay.com/photo/2016/12/10/16/57/shoes-1897708_960_720.jpg',
+    name: 'Bode Wears',
+    description: `Wears at affordable prices. we have the best shoes.`,
+    timestamp: '1 day ago',
+  },
+  {
+    image:
+      'https://cdn.pixabay.com/photo/2017/04/26/16/06/mobile-2262928_960_720.jpg',
+    name: 'Samsung',
+    description: `Keep close to Nature's heart... and break clear away, once in awhile,
+    and climb a mountain or spend a week in the woods. Wash your spirit
+    clean.`,
+    timestamp: '1 day ago',
+  },
+]);
