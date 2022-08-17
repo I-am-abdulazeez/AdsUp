6c062d35e92b834ac2570df6809e357bb21c8205
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-17 15:14:45 +0100

    chore: renamed component

---
+++ src/components/AdsDetails.vue
@@ -0,0 +1,30 @@
+<template>
+  <h4 v-for="ad in adsArray" :key="ad.id">
+    {{ ad.name }}
+  </h4>
+</template>
+
+<script lang="ts">
+import { adsArray } from '@/data/index';
+import { defineComponent, computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+export default defineComponent({
+  name: 'AdsDetails',
+  setup: () => {
+    const route = useRoute();
+    const userId = computed(() => {
+      return route.params.userId;
+    });
+
+    console.log(userId);
+
+    return {
+      userId,
+      adsArray,
+    };
+  },
+});
+</script>
+
+<style scoped></style>
