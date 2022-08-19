807285c1d4f6809fbd1676a15b57f3f1b3137370
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-08-19 17:02:21 +0100

    chore: backButton management in app

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,8 @@ import { defineComponent, onMounted } from 'vue';
 import { App } from '@capacitor/app';
 import { StatusBar, Style } from '@capacitor/status-bar';
 import { SplashScreen } from '@capacitor/splash-screen';
+import { modalController } from '@ionic/core';
+import { useRoute } from 'vue-router';
 
 export default defineComponent({
   name: 'App',
@@ -25,13 +27,31 @@ export default defineComponent({
   },
   setup: () => {
     const ionRouter = useIonRouter();
+    const modalCtrl = modalController;
+    const router = useRoute();
     const setStatusBarStyleDark = async () => {
       await StatusBar.setStyle({ style: Style.Dark });
       await StatusBar.setBackgroundColor({ color: '#121212' });
     };
+
+    useBackButton(0, () => {
+      if (router.path === '/tab/reels') {
+        ionRouter.back();
+      } else {
+        App.exitApp();
+      }
+    });
+
     useBackButton(-1, () => {
       if (!ionRouter.canGoBack()) {
         App.exitApp();
+      }
+    });
+
+    useBackButton(100, async () => {
+      const modal = await modalCtrl.getTop();
+      if (modal) {
+        await modal.dismiss();
       }
     });
     onMounted(() => {
